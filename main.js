c1f10808eaa6e63fef64928f5ecd26c1e7f54e6c
AlexM <1137amartinez@gmail.com>
2024-06-28 00:51:46 -0700

    correct image paths for background and Moon

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-import './style.css'
+import './style.css';
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 // Setup
 const scene = new THREE.Scene();
+
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
 });
@@ -30,11 +33,11 @@ const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
 
 // Helpers
-// const lightHelper = new THREE.PointLightHelper(pointLight)
-// const gridHelper = new THREE.GridHelper(200, 50);
-// scene.add(lightHelper, gridHelper)
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper);
 
-// const controls = new OrbitControls(camera, renderer.domElement);
+const controls = new OrbitControls(camera, renderer.domElement);
 
 function addStar() {
   const geometry = new THREE.SphereGeometry(0.25, 24, 24);
@@ -49,20 +52,21 @@ function addStar() {
   scene.add(star);
 }
 
-Array(200).fill().forEach(addStar);
+Array(5000).fill().forEach(addStar);
 
 // Background
-const spaceTexture = new THREE.TextureLoader().load('./space.jpg');
+const spaceTexture = new THREE.TextureLoader().load('public/space.jpg');
 scene.background = spaceTexture;
 
 // Moon
-const moonTexture = new THREE.TextureLoader().load('./Moon.jpg');
+const moonTexture = new THREE.TextureLoader().load('public/Moon.jpg');
 const moon = new THREE.Mesh(
   new THREE.SphereGeometry(3, 32, 32),
   new THREE.MeshStandardMaterial({
     map: moonTexture,
   })
 );
+
 
 scene.add(moon);
 
@@ -76,8 +80,8 @@ function moveCamera() {
   moon.rotation.y += 0.075;
   moon.rotation.z += 0.05;
 
-  camera.position.z = t * -0.01;
-  camera.position.x = t * -0.0002;
+  camera.position.z = 30 + t * -0.01;
+  camera.position.x = -3 + t * -0.0002;
   camera.rotation.y = t * -0.0002;
 }
 
@@ -94,7 +98,7 @@ function animate() {
 
   moon.rotation.x += 0.005;
 
-  // controls.update();
+  controls.update();
 
   renderer.render(scene, camera);
 }
